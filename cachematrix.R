bf811ed24b625882fa8bc5e2d1727390c3209f15
engineerror420 <65111479+engineerror420@users.noreply.github.com>
2020-09-10 13:49:09 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -17,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
     
   }
   get <- function() x
-  setinverse <- function(solve) m<<-solve
+  setinverse <- function(inverse) m<<-inverse
   getinverse <- function() m
   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
@@ -39,7 +39,7 @@ if(!is.null(m)) {
   
   }
   data<-x$get
-  m<-mean(data, ...)
+  m<-solve(data, ...)
   x$setinverse(m)
   m
 
